dfd24cbd495dec99829336393aee23b7d61a4a70
Spyros Paterakis <sppaterak@gmail.com>
2022-04-23 00:26:40 +0300

    refactor musiciansDto

--- CretanMusicians.API/Models/MusicianDto/MusicianDto.cs
+++ CretanMusicians.API/Models/MusicianDto/MusicianDto.cs
@@ -1,13 +1,7 @@
-﻿using CretanMusicians.API.Data;
-
-namespace CretanMusicians.API.Models.MusicianDto
+﻿namespace CretanMusicians.API.Models.MusicianDto
 {
     public class MusicianDto : BaseMusicianDto
     {
         public int Id { get; set; }
-
-        public Origin origin { get; set; }
-
-        public Instrument instrument { get; set; }
     }
 }
